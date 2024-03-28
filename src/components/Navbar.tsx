e466049898fd9e251f43b29d42589f292e469238
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-29 00:18:43 +0530

    tejas

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,17 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
 
 
 const Navbar = () => {
@@ -20,32 +31,42 @@ const Navbar = () => {
           <ul className="hidden md:flex space-x-4 dark:text-white">
             <li><a href="#" className="hover:dark:text-gray-400  hover:text-black">Home</a></li>
             <li><a href="#" className="hover:dark:text-gray-400  hover:text-black">About</a></li>
-            <li><a href="#" className="hover:dark:text-gray-400  hover:text-black">Services</a></li>
+            <li><NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>Tchnologies</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <NavigationMenuLink>Link</NavigationMenuLink>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+            </li>
             <li><a href="#" className="hover:dark:text-gray-400  hover:text-black">Contact</a></li>
           </ul>
         </div>
         <div>
           <ul className='hidden md:flex flex-row space-x-4 dark:text-white'>
             <li><button>signin</button></li>
-            <li><ModeToggle />
-            </li>
+            <li><ModeToggle /></li>
 
           </ul>
         </div>
-        <DropdownMenu>
-  <DropdownMenuTrigger><button id="menu-toggle" className="md:hidden right-0 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white">
-          <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16v12H4zm-2-2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"></path></svg>
-        </button></DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    <DropdownMenuItem>Profile</DropdownMenuItem>
-    <DropdownMenuItem>Billing</DropdownMenuItem>
-    <DropdownMenuItem>Team</DropdownMenuItem>
-    <DropdownMenuItem>Subscription</DropdownMenuItem>
-  </DropdownMenuContent>
-</DropdownMenu>
-        
+        <div className='flex md:hidden'>
+          <DropdownMenu>
+            <DropdownMenuTrigger><button id="menu-toggle" className="md:hidden right-0 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white">
+              <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16v12H4zm-2-2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"></path></svg>
+            </button></DropdownMenuTrigger>
+            <DropdownMenuContent className='mt-1 dark:bg-black md:hidden'>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
     </nav>
   )
