e3e99a11e689de68bb3461251f09532ad336791b
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-30 23:54:31 +0530

    tejas

--- src/pages/Chatwithpdf.tsx
+++ src/pages/Chatwithpdf.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { FormEventHandler, useState } from 'react'
 
+import { Button } from "@/components/ui/button"
 
 
 
 const chatwithpdf = () => {
+    const [files,setFile] = useState<any>([])
     const [messages, setMessages] = useState<{ text: string; fromUser: boolean }[]>([{ text: "dfvhdvfvfhgv", fromUser: false }]);
     const [inputText, setInputText] = useState('');
 
@@ -18,7 +20,12 @@ const chatwithpdf = () => {
 
     };
 
-const onsubmit = ()=>{
+const onsubmit = async  (e:any)=>{
+    e.preventDefault()
+const formdata = new FormData ()
+formdata.append("file",files)
+console.log(files)
+ // uploading to appwrite
 
 }
 
@@ -26,10 +33,11 @@ const onsubmit = ()=>{
     return (
         <div className='w-full h-screen flex flex-row gap-2 md:px-40 px-3 bg-monu dark:text-white text-black '>
             <div className='bg-blue-800 hidden p-4 md:w-1/3 md:flex md:flex-col gap-8 items-center '>
-                <form action="onsubmit">
+                <form onSubmit={onsubmit}>
                     <label className="block mb-2 mt-12 font-medium text-gray-900 dark:text-white text-2xl text-center" htmlFor="multiple_files">Upload multiple PDF files</label>
-                    <input className="block w-full text-sm  text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="multiple_files" type="file" multiple />
-                        </form>
+                    <input className="block w-full text-sm  text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"  onChange={(e)=>{setFile(e.target.files)}}  id="multiple_files" type="file" multiple />
+                    <button type="submit"  className="mt-4 mx-auto dark:text-white bg-green-700 hover:bg-green-800  font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+</form>
                 <div className='0 w-full flex flex-col items-center gap-4'><h3 className='text-2xl font-semibold mt-11' id='previous-session'>previous sesions</h3>
                     <div id='previous-session-list' className=' w-full bg-green-500 rounded-xl p-2'>tejaswee kumar singh</div>
                 </div>
@@ -41,7 +49,7 @@ const onsubmit = ()=>{
                 <div className="flex flex-col  h-full w-full relative overflow-y-scroll">
                     {messages.map((value, index) => {
                         if (value.fromUser === true) {
-                            return <div className=' mt-2 md:mt-7 flex w-full p-3 rounded-md justify-start items-center bg-white dark:text-white text-black text-base md:text-xl  '>User :  {value.text}</div>
+                            return <div key={index} className=' mt-2 md:mt-7 flex w-full p-3 rounded-md justify-start items-center bg-white dark:text-white text-black text-base md:text-xl  '>User :  {value.text}</div>
                         }
                         else {
 
