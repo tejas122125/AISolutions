32368973b13b7eb5cf748539790d8c4a9b1ae590
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-31 00:01:56 +0530

    tejas

--- src/pages/Chatwithpdf.tsx
+++ src/pages/Chatwithpdf.tsx
@@ -5,14 +5,14 @@ import { Button } from "@/components/ui/button"
 
 
 const chatwithpdf = () => {
-    const [files,setFile] = useState<any>([])
+    const [files, setFile] = useState<any>([])
     const [messages, setMessages] = useState<{ text: string; fromUser: boolean }[]>([{ text: "dfvhdvfvfhgv", fromUser: false }]);
     const [inputText, setInputText] = useState('');
 
     const handleSendMessage = () => {
-        const input:HTMLElement = document.getElementById('textinput')!
+        const input: HTMLElement = document.getElementById('textinput')!
         input.innerText = ''
-        
+
         if (inputText.trim() === '') return;
         setMessages([...messages, { text: inputText, fromUser: true }]);
 
@@ -20,14 +20,14 @@ const chatwithpdf = () => {
 
     };
 
-const onsubmit = async  (e:any)=>{
-    e.preventDefault()
-const formdata = new FormData ()
-formdata.append("file",files)
-console.log(files)
- // uploading to appwrite
+    const onsubmit = async (e: any) => {
+        e.preventDefault()
+        const formdata = new FormData()
+        formdata.append("file", files)
+        console.log(files)
+        // uploading to appwrite
 
-}
+    }
 
 
     return (
@@ -35,9 +35,9 @@ console.log(files)
             <div className='bg-blue-800 hidden p-4 md:w-1/3 md:flex md:flex-col gap-8 items-center '>
                 <form onSubmit={onsubmit}>
                     <label className="block mb-2 mt-12 font-medium text-gray-900 dark:text-white text-2xl text-center" htmlFor="multiple_files">Upload multiple PDF files</label>
-                    <input className="block w-full text-sm  text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"  onChange={(e)=>{setFile(e.target.files)}}  id="multiple_files" type="file" multiple />
-                    <button type="submit"  className="mt-4 mx-auto dark:text-white bg-green-700 hover:bg-green-800  font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
-</form>
+                    <input className="block w-full text-sm  text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" onChange={(e) => { setFile(e.target.files) }} id="multiple_files" type="file" multiple />
+                    <button type="submit" className="mt-4 mx-auto dark:text-white bg-green-700 hover:bg-green-800  font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+                </form>
                 <div className='0 w-full flex flex-col items-center gap-4'><h3 className='text-2xl font-semibold mt-11' id='previous-session'>previous sesions</h3>
                     <div id='previous-session-list' className=' w-full bg-green-500 rounded-xl p-2'>tejaswee kumar singh</div>
                 </div>
--- src/utils/appwrite/funtions.ts
+++ src/utils/appwrite/funtions.ts
@@ -1,2 +1,19 @@
 // uploading pdf files in appwrite 
 
+import { client, storage } from "./confis"
+import { ID } from "appwrite"
+const uploadpdf = async( file : File)=>{
+    const clt:any = client
+    const stg:any = storage
+    try {
+        const response = await storage.createFile(
+            '658da6ec42519f39311a',
+            ID.unique(),
+            file
+        )
+        return  response
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
