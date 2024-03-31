fc0bf97f85d90db07e482998604f1245606d048e
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-31 13:25:10 +0530

    tejas

--- backend/python/chatwithpdf.py
+++ backend/python/chatwithpdf.py
@@ -4,6 +4,7 @@ from PyPDF2 import PdfReader
 from operator import itemgetter
 from langchain_community.document_loaders import PyPDFLoader
 from dotenv import load_dotenv
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
@@ -14,10 +15,12 @@ from langchain.chains import ConversationChain
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_core.messages import AIMessage, HumanMessage, get_buffer_string
-from langchain_core.prompts import format_document
-from langchain_core.runnables import RunnableParallel
-from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
 from langchain.prompts.prompt import PromptTemplate
+
 
 load_dotenv()
 openaikey = os.environ.get("OPENAI_API_KEY")
@@ -53,19 +56,6 @@ def get_vectorstore(text_chunks):
 
 def get_conversational_chain(vectorstore):
     llm = ChatOpenAI(temperature=0.2,api_key=openaikey)
-    # # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-    # memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-    # conversation_chain = conversational_retrieval(
-    #     llm=llm,
-    #     retriever=vectorstore.as_retriever(),
-    #     memory=memory
-    # )
-    # return conversation_chain
-    
-    # retriever = VectorStoreRetriever(vectorstore=FAISS(...))
-    # retrievalQA = RetrievalQA.from_llm(llm=OpenAI(), retriever=retriever)
-
-    
     template = """Answer the question based only on the following context:
     {context}   
 
@@ -80,54 +70,31 @@ def get_conversational_chain(vectorstore):
     )
     return chain
     
-def question_with_memory(vectorstore):
-    retriever = vectorstore.as_retriever()
-    
-    _template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
-
-    Chat History:
-    {chat_history}
-    Follow Up Input: {question}
-    Standalone question:"""
-    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
-    template = """Answer the question based only on the following context:
-    {context}
-
-    Question: {question}
-    """
-    ANSWER_PROMPT = ChatPromptTemplate.from_template(template)
-    DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
 
 
-    def _combine_documents(
-        docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"
-    ):
-        doc_strings = [format_document(doc, document_prompt) for doc in docs]
-        return document_separator.join(doc_strings)    
 
-    _inputs = RunnableParallel(
-    standalone_question=RunnablePassthrough.assign(
-        chat_history=lambda x: get_buffer_string(x["chat_history"])
-    )
-    | CONDENSE_QUESTION_PROMPT
-    | ChatOpenAI(temperature=0)
-    | StrOutputParser(),
-    )
-    _context = {
-    "context": itemgetter("standalone_question") | retriever | _combine_documents,
-    "question": lambda x: x["standalone_question"],
-    }
-    conversational_qa_chain = _inputs | _context | ANSWER_PROMPT | ChatOpenAI()
 
     
 def main():
-   
+    chat_history =[
+        HumanMessage(content="hello"),
+        AIMessage(content="hello i am a excellent rag bot")
+        
+    ] 
     raw_text = get_pdf_text("monu.pdf")
     text_chunks = get_text_chunks(raw_text)
-    vectore_store = get_vectorstore(text_chunks) 
-    conversation_chain = get_conversational_chain(vectore_store)   
-    response  = conversation_chain.invoke("List out the marketing strategies of Blackberry.")
-    print (response)
+    vector_store = get_vectorstore(text_chunks) 
+    # conversation_chain = get_conversational_chain(vector_store)   
+    # response  = conversation_chain.invoke("List out the marketing strategies of Blackberry.")
+    # print (response)
+    conversational_chain = get_context_retriever_chain(vector_store)
+    rag_chain = get_conversational_rag_chain(conversational_chain)
+    response = rag_chain.invoke({
+    "chat_history":chat_history,
+    "input": "List out the marketing strategies of Blackberry."
+    })
+    print(response)
+    
 
 
 if __name__ == '__main__':
