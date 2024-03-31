eaf2a7a311af448f06ec5dec7c0a75124574ce87
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-31 11:26:58 +0530

    tejas

---
+++ backend/python/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+
+@app.route('/pdffiles', methods=['GET'])
+def get_array(files):
+    print(files)
+
+if __name__ == '__main__':
+    app.run(debug=True)
