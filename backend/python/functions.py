ee48588abe365e43b4a9a14227303077f371c23b
tejas122125 <tejasweekumarsingh@gmail.com>
2024-03-31 12:59:11 +0530

    tejas

---
+++ backend/python/functions.py
@@ -0,0 +1,16 @@
+
+import requests
+
+
+def download_pdf(url, filename):
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Open the file in binary write mode and write the response content (PDF data) to it
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+        print(f"PDF downloaded successfully and saved as '{filename}'")
+    else:
+        print(f"Failed to download PDF. Status code: {response.status_code}")
--- backend/python/main.py
+++ backend/python/main.py
@@ -1,23 +1,13 @@
 from flask import Flask, jsonify,request
 from flask_cors import CORS, cross_origin
-import requests
+
+from backend.python.functions import download_pdf
 
 
 app = Flask(__name__)
 CORS(app)
 
-def download_pdf(url, filename):
-    # Send a GET request to the URL
-    response = requests.get(url)
-    
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Open the file in binary write mode and write the response content (PDF data) to it
-        with open(filename, 'wb') as file:
-            file.write(response.content)
-        print(f"PDF downloaded successfully and saved as '{filename}'")
-    else:
-        print(f"Failed to download PDF. Status code: {response.status_code}")
+
 
 @app.route('/pdffiles', methods=['POST'])
 def getlink():
