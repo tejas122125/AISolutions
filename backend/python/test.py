a5b07c8116a54f6620f5aa9ba2922fa6b8fd1bc8
tejas122125 <tejasweekumarsingh@gmail.com>
2024-04-01 16:19:12 +0530

    tejas

--- backend/python/test.py
+++ backend/python/test.py
@@ -7,10 +7,9 @@ def connect_to_mongodb (connectionstring = "mongodb+srv://tejasweekumarsingh:vex
     return client
 
 
-def create_chathistory (client,email='tejaswee@gmail.com',userchat='',airesponse=''):
-    db = client['testdatabase']
-    # Access a collection
-    collection = db['userchats']
+def create_chathistory (client,database = "testdatabase",collection='userchats',email='tejaswee@gmail.com',userchat='',airesponse=''):
+    db = client[database]
+    collection = db[collection]
     document = collection.find_one({'email': email})
 
     if document:
@@ -30,11 +29,34 @@ def create_chathistory (client,email='tejaswee@gmail.com',userchat='',airesponse
         
         print("created a new document")
 
-def 
+def get_chathistory(client,database="testdatabase",collection='userchats',email="tejaswee@gmail.com"):
+    userquestion = []
+    airesponse = []
+    # Access a database
+    db = client[database]
+
+# Access a collection
+    collection = db[collection]
+    filter = {'email': 'tejaswee@gmail.com'}
+
+# Retrieve the document
+    document = collection.find_one(filter)
+
+# Print the document
+    if document:
+        userquestion = document.get('userquestion')
+        airesponse = document.get('airesponse')
+        return userquestion,airesponse
+        
+    else:
+        return "fgfgf",""
+        print('Document not found')
 
 def main ():
     client  = connect_to_mongodb()
-    create_chathistory(client,userchat="monujinda bad")
+    # create_chathistory(client,userchat="monujinda dffrgrgbad",airesponse="haiahaiah")
+    userquestion,airesponse = get_chathistory(client=client)
+    print(userquestion,airesponse)
 
 # if __name__ == '__main__':
 #     main()    
