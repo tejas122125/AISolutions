16076edb8dcbcfa990ef9f7b3fa5dcf528c03e52
tejas122125 <tejasweekumarsingh@gmail.com>
2024-04-01 16:00:46 +0530

    tejas

--- backend/python/test.py
+++ backend/python/test.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 
 def connect_to_mongodb (connectionstring = "mongodb+srv://tejasweekumarsingh:vex2l2htbIlbnGjD@tejas.tokgflw.mongodb.net/"):
-    client = MongoClient('mongodb://localhost:27017/')
+    client = MongoClient(connectionstring)
     return client
 
 
@@ -15,18 +15,32 @@ def create_chathistory (client,email='tejaswee@gmail.com',userchat='',airesponse
 
     if document:
         # Update the array in the document
-        new_elements = [userchat]
-        updated_array = document.get('userquestion', []) + new_elements
+        
+        updated_userquestion = document.get('userquestion', []) + [userchat]
+        updated_airesponse = document.get('airesponse', []) + [airesponse]
+        
     
         # Update the document in the collection
-        collection.update_one({'email': email}, {'$set': {'userquestion': updated_array}})
+        collection.update_one({'email': email}, {'$set': {'userquestion': updated_userquestion ,'airesponse': updated_airesponse}})
         print('Document updated successfully')
     else:
         print('Document not found for email:', email)
-        document = {'email':email, 'userquestion': [userchat]}
+        document = {'email':email, 'userquestion': [userchat],"airesponse":[airesponse]}
+        collection.insert_one(document)
+        
         print("created a new document")
 
-# Insert a document with email and array of strings
-    # document = {'email': 'example@example.com', 'userquestion': ['string1', 'string2', 'string3']}
-    collection.insert_one(document)
-    
+def 
+
+def main ():
+    client  = connect_to_mongodb()
+    create_chathistory(client,userchat="monujinda bad")
+
+# if __name__ == '__main__':
+#     main()    
+
+
+
+
+
+main()
