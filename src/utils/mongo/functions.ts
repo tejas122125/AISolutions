167230840efa6d9a6c5135fc50ec7b356f2f44d8
tejas122125 <tejasweekumarsingh@gmail.com>
2024-04-02 19:49:40 +0530

    tejas

--- src/utils/mongo/functions.ts
+++ src/utils/mongo/functions.ts
@@ -33,24 +33,28 @@ export const uploadDocument =  async (token:string,msgid:string,msgname:string)=
 
 const getChatWithPdfMessages = async (token:string,messageid:string) => {
 
-    const uri = import.meta.env.VITE_APP_MONGOSTRING;
-    const client = new MongoClient(uri);
+    // const uri = import.meta.env.VITE_APP_MONGOSTRING;
+    // const uri  =  process.env.VITE_APP_MONGOSTRING;
+    const client = new MongoClient("mongodb+srv://tejasweekumarsingh:vex2l2htbIlbnGjD@tejas.tokgflw.mongodb.net/");
 
     try {
         await client.connect();
 
-        const database = client.db('testdatabse'); 
+        const database = client.db('testdatabase'); 
         const collection = database.collection('chatwithpdf'); 
-        const document = await collection.findOne({
-            "token":token,
-            "messageid":messageid
-        });
-        const arrayData = document!.human; 
 
-        console.log('Array from document:', arrayData);
+        const filter = {token:token,messageid:messageid }
+        const document =  await collection.findOne(filter)
+        console.log(document)
+        const human = document!.human; 
+        const ai = document!.ai;
+        return  [human, ai];
+        // console.log('Array from document:', human);
     } catch (error) {
         console.error('Error occurred:', error);
     } finally {
         await client.close(); // Close the MongoDB connection
     }
 }
+
+getChatWithPdfMessages("monu","monu")
