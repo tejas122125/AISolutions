097678b953e564d45141bfdfe9f858b5df0b0706
tejas122125 <tejasweekumarsingh@gmail.com>
2024-04-07 15:31:47 +0530

    nfkjgn

--- src/pages/Chatwithpdf.tsx
+++ src/pages/Chatwithpdf.tsx
@@ -51,13 +51,12 @@ const chatwithpdf = () => {
         "plum",
         "apricot"
     ]
-    let human = []
-    let ai = []
-    const [newchat, setNewChat] = useState<boolean>(false)
 
+    const [newchat, setNewChat] = useState<boolean>(false)
+    const [ai, setai] = useState<string[]>([])
+    const [human, sethuman] = useState<string[]>([])
     const [pdffiles, sePdftFiles] = useState<File[]>([])
     const [currentMessages, setCurrentMessages] = useState<boolean>(false)
-    const [msgid, setMsgId] = useState<string>("")
     const [previousSession, setPreviousSession] = useState<boolean>(false)
     const [messageids, setMessageIds] = useState<string[]>([])
     const [messageNames, setMessageNames] = useState<string[]>([])
@@ -73,13 +72,15 @@ const chatwithpdf = () => {
     const getting = async (token: string) => {
         console.log("second")
         try {
+           
             const temp = await getAllChatWithPdf("monu")
-console.log(temp)
             setMessageIds(temp[0])
             setMessageNames(temp[1])
             setPreviousSession(true)
-            console.log(messageNames)
-
+            setCurrentSessionId(messageids[messageids.length - 1])
+const curr = temp[0][temp[0].length -1]
+console.log("hgfdhgdh",curr)
+            getids(token, curr)
         } catch (error) {
             console.log(error)
         }
@@ -88,21 +89,19 @@ console.log(temp)
     const newChatHandle = async (files: File[], messagename: string) => {
         console.log("fourth")
         // upload and get the array of file ids
-        const token = "monu"
         console.log(files)
         const fileids = await uploadPdf(files)
         console.log(fileids)
         // handle and give file ids to downloading the files
         if (fileids) {
             setSubmitted(true)
-
             const msgid = generateRandomString(6)
             setMessageNames((prev) => { return [...prev, messagename] })
             setMessageIds((prev) => { return [...prev, msgid] })
             setCurrentSessionId(msgid)
             const uploadres = await uploadChatWithPdf(token, msgid, messagename, fileids)
             if (uploadres) {
-                handleCurrentSessionMessages(fileids)
+                handleCurrentSessionMessages(fileids,msgid)
             }
 
 
@@ -114,36 +113,37 @@ console.log(temp)
     }
 
 
-    const handleCurrentSessionMessages = async (fileids: string[]) => {
+    const handleCurrentSessionMessages = async (fileids: string[] ,currid:string) => {
         // call the backend to download the files and be ready
-        let reslink = []
-        for (let i = 0; i < fileids.length; i++) {
-            const link = getDownloadLink(fileids[i])
-            reslink.push(link.href)
-        }
-        const data = {
-            "pdfid": currentSessionId,
-            "downloadlink": reslink
-        }
+        // let reslink = []
+        // for (let i = 0; i < fileids.length; i++) {
+        //     const link = getDownloadLink(fileids[i])
+        //     reslink.push(link.href)
+        // }
+        // const data = {
+        //     "pdfid": currentSessionId,
+        //     "downloadlink": reslink
+        // }
 
-        try {
-            const response = await axios.post("http://127.0.0.1:5000/uploadpdffiles", data)
-            console.log(response)
-            if (response.data) {
-                //uploaded and downloaded file in backend
-                setDOwnloaded(true)
-            }
+        // try {
+        //     const response = await axios.post("http://127.0.0.1:5000/uploadpdffiles", data)
+        //     console.log(response)
+        //     if (response.data) {
+        //         //uploaded and downloaded file in backend
+        //         setDOwnloaded(true)
+        //     }
 
-        } catch (error) {
-            console.log(error)
-        }
-
+        // } catch (error) {
+        //     console.log(error)
+        // }
+        console.log("nindb", currid)
 
         try {
             const token = "monu"
-            const msg = await getChatWithPdfMessages(token, currentSessionId)
-            human = msg[0]
-            ai = msg[1]
+            const msg = await getChatWithPdfMessages(token, currid)
+            console.log(msg)
+            sethuman(msg[0])
+            setai(msg[1])
             //change in ui how to set ai and human messages
             setCurrentMessages(true)
         } catch (error) {
@@ -152,34 +152,29 @@ console.log(temp)
 
     }
 
-    const getids = async (token: string, currentSessionId: string) => {
-        const fileids = await getFileIds(token, currentSessionId)
-        handleCurrentSessionMessages(fileids)
+    const getids = async (token: string, currssid: string) => {
+        const fileids = await getFileIds(token, currssid)
+        console.log("tejas",currssid)
+        console.log(fileids)
+        handleCurrentSessionMessages(fileids,currssid)
 
     }
+
+
+    //dfkiufdheuifhufrguyefdgeuyfdveuyf
     useEffect(() => {
         const token = "monu"
-        console.log("first")
         getting(token)
-        if (previousSession) {
-            console.log("thitrd")
-            setCurrentSessionId(messageids[-1])
-            // get all file ids from thje tokenn and messageids
-            getids(token, currentSessionId)
-        }
+        // console.log("first")
+        // if (previousSession) {
+        //     console.log("thitrd")
+        //     setCurrentSessionId(messageids[-1])
+        //     // get all file ids from thje tokenn and messageids
+        //     getids(token, currentSessionId)
+        // }
+    }, [])
 
 
-
-    }, [setPreviousSession, setMessageIds, setMessageNames])
-
-    const displayChat = () => {
-
-        const user = []
-        const ai = []
-
-
-
-    }
     const handleSendMessage = async () => {
 
         console.log(inputText)
@@ -222,6 +217,7 @@ console.log(temp)
     let msgname = ""
     let pdffile: any = []
     const onSubmit = async (values: any) => {
+        setNewChat(false)
         newChatHandle(pdffile, values.chatname)
         // console.log("first")
         // console.log(values)
@@ -327,14 +323,15 @@ console.log(temp)
                     </div>
                     <h3 className='text-2xl bg-slate-500 w-full text-center p-2 rounded-md mt-11' id='previous-session'>Previous Sesions</h3>
 
-                   {previousSession && <div className='0 w-full flex flex-col items-center md:gap-4 gap-2 h-full  overflow-y-scroll  overflow-x-hidden'>
-                        {messageNames.map((value,index) => {
+                    {previousSession && <div className='0 w-full flex flex-col items-center md:gap-4 gap-2 h-full  overflow-y-scroll  overflow-x-hidden'>
+                        {messageNames.map((value, index) => {
                             return <button id='previous-session-list' key={index} className=' w-full md:mt-4 mt-4 transition-transform duration-300 transform  hover:scale-110  backdrop-blur-md bg-slate-200/60 rounded-xl p-2 hover:font-semibold text-center hover:bg-green-500/50' onClick={async () => {
                                 const name = value;
                                 const id = messageids[messageNames.indexOf(name)]
+                                console.log("clicked",id)
                                 const ids = await getFileIds(token, id)
-                                handleCurrentSessionMessages(ids)
-                            }} >{value}</button>
+                                handleCurrentSessionMessages(ids,id)
+                            }} >{value}{messageids[index]}</button>
                         })}
                     </div>}
 
@@ -342,15 +339,11 @@ console.log(temp)
                 <div className='bg-purple-800 w-full h-full p-2 relative'>
 
                     <div className="flex flex-col  h-full w-full relative overflow-y-scroll">
-                        {messages.map((value, index) => {
+                        {human.map((value, index) => {
+                            return <div className='w-full h-fit flex-col flex bg-blue-400'> <div key={index} className=' mt-2 md:mt-7 flex w-full p-3 rounded-md justify-start items-center bg-white dark:text-white text-black text-base md:text-xl  '>User :  {value}</div>
 
-                            if (value.fromUser === true) {
-                                return <div key={index} className=' mt-2 md:mt-7 flex w-full p-3 rounded-md justify-start items-center bg-white dark:text-white text-black text-base md:text-xl  '>User :  {value.text}</div>
-                            }
-                            else {
-
-                                return <div key={index} className=' flex w-full p-3 rounded-md justify-end items-center dark:text-white text-black text-base md:text-xl  '>AI :  {value.text}</div>
-                            }
+                                <div key={index} className=' flex w-full p-3 rounded-md justify-end items-center dark:text-white text-black text-base md:text-xl  '>AI :  {ai[index]}</div>
+                            </div>
 
                         })
 
