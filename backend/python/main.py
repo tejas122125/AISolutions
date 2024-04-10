7bd6049c72953fc7faf69e47b5775ba9d42e433c
tejas122125 <tejasweekumarsingh@gmail.com>
2024-04-10 23:41:21 +0530

    tejaswee

--- backend/python/main.py
+++ backend/python/main.py
@@ -128,7 +128,7 @@ def download_pdf(url, filename):
     # Send a GET request to the URL
     response = requests.get(url)
     
-    # Check if the request was successful (status code 200)
+
     if response.status_code == 200:
         # Open the file in binary write mode and write the response content (PDF data) to it
         if not os.path.exists(filename):
